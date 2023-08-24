f9fec375aafb2d1dbcadc767974b7ea936d676f1
Hubert Deng <hubertdeng123@gmail.com>
2023-08-24 13:47:09 -0700

    attempt to trigger an error

--- src/a.py
+++ src/a.py
@@ -1,6 +1,6 @@
 ##
 # ![[ChangeTogether.Start]]
 # ![[ChangeTogether.With("/docs/b.md")]]
-print("We are in python now")
+print("We are in python now, changed from before")
 # ![[ChangeTogether.End]]
 ##
